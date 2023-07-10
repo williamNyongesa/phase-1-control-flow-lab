0d976c8cda9a593ca203e9e9b39b9723378e89d4
williamNyongesa <marakawilliam@gmail.com>
2023-07-10 19:45:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <2501){
+    return 'I will gladly take your thirty bucks.';
+  }else if (feet > 2500) {
+    return 'No can do.';
+  }else {
+    return 'This ride will cost you.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? 'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
